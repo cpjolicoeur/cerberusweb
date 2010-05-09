108b4644b7bcf161a281ef81935117e4f214bb63
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-10 03:14:10 +0800

    Added some very basic build status.

--- cerberusweb.rb
+++ cerberusweb.rb
@@ -21,6 +21,7 @@ end
 class CerberusProject
   attr_accessor :name
   attr_accessor :config
+  attr_accessor :status
 
   def feed
     return '' unless config && config["publisher"] && config["publisher"]["rss"] && config["publisher"]["rss"]["file"]
@@ -34,12 +35,22 @@ class CerberusProject
       
   end
 
+  def build_success?
+    status && status["successful"]
+  end
+
+  def built_at
+    status && status["timestamp"]
+  end
+
   def self.find(name)
     return nil unless File.exists?(ENV['HOME'] + "/.cerberus/config/#{name}.yml")
 
     project = new
     project.name = name
     project.config = YAML.load(File.read(ENV['HOME'] + "/.cerberus/config/#{name}.yml"))
+    status_log = ENV['HOME'] + "/.cerberus/work/#{name}/status.log"
+    project.status = YAML.load(File.read(status_log)) if File.exists?(status_log)
     project
   end
 
@@ -48,11 +59,9 @@ class CerberusProject
       acc << File.basename(config_file).gsub('.yml','')
     end
 
-    project_names.inject([]) do |projects, name|
-      p = new
-      p.name = name
-      projects << p
+    project_names.inject([]) do |acc, name|
+      acc << find(name)
     end
-    
+
   end
 end

ce67dfe6564ecb1b405db23cea48d584bdab33d4
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-10 02:36:13 +0800

    Added server errors.

--- cerberusweb.rb
+++ cerberusweb.rb
@@ -6,6 +6,11 @@ get '/' do
   erb :index
 end
 
+get '/server_errors' do
+  @error_log = File.read(ENV['HOME'] + '/.cerberus/error.log')
+  erb :server_errors
+end
+
 def parse_cerberus_list
   Dir[ENV['HOME'] + '/.cerberus/config/*.yml'].inject([]) do |projects, config_file|
     projects << File.basename(config_file).gsub('.yml','')

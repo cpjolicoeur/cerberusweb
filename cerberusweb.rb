de91390b95e3676ba44c80041ba196901438eeca
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-10 03:00:02 +0800

    Load feeds through the web interface.

--- cerberusweb.rb
+++ cerberusweb.rb
@@ -1,9 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 
+require 'yaml'
+
 get '/' do
   @projects = CerberusProject.load
   erb :index
+end
+
+get '/projects/:project_name/feed' do
+  @project = CerberusProject.find(params[:project_name])
+  @project.feed
 end
 
 get '/server_errors' do
@@ -13,6 +20,28 @@ end
 
 class CerberusProject
   attr_accessor :name
+  attr_accessor :config
+
+  def feed
+    return '' unless config && config["publisher"] && config["publisher"]["rss"] && config["publisher"]["rss"]["file"]
+
+    file = config["publisher"]["rss"]["file"]
+    if File.exists?(file)
+      File.read(file)
+    else
+      ''
+    end
+      
+  end
+
+  def self.find(name)
+    return nil unless File.exists?(ENV['HOME'] + "/.cerberus/config/#{name}.yml")
+
+    project = new
+    project.name = name
+    project.config = YAML.load(File.read(ENV['HOME'] + "/.cerberus/config/#{name}.yml"))
+    project
+  end
 
   def self.load
     project_names = Dir[ENV['HOME'] + '/.cerberus/config/*.yml'].inject([]) do |acc, config_file|

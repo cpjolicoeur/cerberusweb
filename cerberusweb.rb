8b09c906593f470ad00880aa83fb163ed003e33b
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-10 02:31:00 +0800

    Added simple project list

--- cerberusweb.rb
+++ cerberusweb.rb
@@ -2,5 +2,13 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "Hello world"
+  @projects = parse_cerberus_list
+  erb :index
+end
+
+def parse_cerberus_list
+  Dir[ENV['HOME'] + '/.cerberus/config/*.yml'].inject([]) do |projects, config_file|
+    projects << File.basename(config_file).gsub('.yml','')
+  end.sort
+
 end
